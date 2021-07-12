9351ef2fb6755494712049a9a83b63c2db0d3ed7
Luis Alberto Pérez Coello <luisalbertodev@gmail.com>
2021-07-12 15:52:24 -0500

    added setting of database

---
+++ src/api/config/database.ts
@@ -0,0 +1,32 @@
+import Mongoose from 'mongoose';
+import env from '@env';
+
+let database: Mongoose.Connection;
+
+export const connect = () => {
+  // add your own uri below
+  const uri = env.MONGO_URI;
+  if (database) {
+    return;
+  }
+  Mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  });
+  database = Mongoose.connection;
+  database.once('open', async () => {
+    console.log('Connected to database');
+  });
+  database.on('error', () => {
+    console.log('Error connecting to database');
+  });
+};
+
+export const disconnect = () => {
+  if (!database) {
+    return;
+  }
+  Mongoose.disconnect();
+};
